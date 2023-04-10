76d36903013ab5f040fb0433799300ff7e43e19c
Lyudmila Karpenko <lk@example.com>
2023-04-10 09:37:03 +0300

    Add main program

---
+++ Finaltest/Program.cs
@@ -0,0 +1,76 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры,
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+Commands();
+string[] array = new string[] {};
+
+string command = ReadInput("Введите команду: ");
+switch (command)
+{
+    case "1":
+        array = new string[] { "Hello", "2", "world", ":-)" };
+        break;
+    case "2":
+        array = new string[] { "1234", "1567", "-2", "computer science" };
+        break;
+    case "3":
+        array = new string[] { "Russia", "Denmark", "Kazan" };
+        break;
+    default:
+        Console.WriteLine($"{command} - Такой команды нет");
+        break;
+}
+
+
+int lenNewArray = 0;
+for (int i = 0; i <= array.Length - 1; i++)
+{
+    if (array[i].Length <= 3) lenNewArray++;
+}
+
+string[] newArray = new string[lenNewArray];
+int idx = 0;
+
+for (int i = 0; i <= array.Length - 1; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        newArray[idx] = array[i];
+        idx++;
+    }
+}
+
+PrintArray(array);
+Console.Write(" → ");
+PrintArray(newArray);
+
+void Commands()
+{
+    Console.WriteLine();
+    Console.WriteLine("List of commands:");
+    Console.WriteLine("1 – array: [“Hello”, “2”, “world”, “:-)”]");
+    Console.WriteLine("2 – array: [“1234”, “1567”, “-2”, “computer science”]");
+    Console.WriteLine("3 – array: [“Russia”, “Denmark”, “Kazan”]");
+    Console.WriteLine();
+}
+
+string ReadInput(string msg)
+{
+    Console.Write(msg);
+    return Console.ReadLine();
+}
+
+//  Функция вывода массива в терминал
+void PrintArray(string[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"“{array[i]}”, ");
+    }
+    Console.Write("] ");
+}
